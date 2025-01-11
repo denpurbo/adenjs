7e1097995b1dde80ca4b0296f0ba0b5fcfc1f911
den purbo <91820256+denpurbo@users.noreply.github.com>
2025-01-11 11:36:08 +0700

    Add files via upload

---
+++ utils/composables/socials.ts
@@ -0,0 +1,27 @@
+export const socials = [
+  {
+    name: "GitHub",
+    icon: "mdi:github",
+    url: "https://github.com/denpurbo",
+  },
+  {
+    name: "Spotify",
+    icon: "mdi:spotify",
+    url: "https://open.spotify.com/u",
+  },
+  {
+    name: "Reddit",
+    icon: "mdi:reddit",
+    url: "https://www.reddit.com/",
+  },
+  {
+    name: "Discord",
+    icon: "mdi:discord",
+    url: "https://discord.com/users/827231418308231218",
+  },
+  {
+    name: "Twitter",
+    icon: "mdi:twitter",
+    url: "https://twitter.com/donnysuhendraa",
+  },
+];
